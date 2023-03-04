b441d55fd054a8a3ce9a45642de13ac106dcc64f
JangDeukchun <rlaejrqo465@naver.com>
2023-03-04 15:04:09 +0900

    왜이리 빨간줄이 뜰까

---
+++ project-name/src/test.ts
@@ -0,0 +1,22 @@
+// interface User {
+//     name: string;
+//     age: number;
+//   }
+
+// class User {
+//     constructor(name: string, age: number) {}
+// };
+
+
+// const user: User = {
+//   name: 'Dexter',
+//   age: 21,
+// }
+
+// function getLength(obj: string | string[]) {
+//     return obj.length;
+// }
+
+// function identity(arg: any){ // 호출되는 시점에 타입이 결정된다.
+//     return arg;
+// }
